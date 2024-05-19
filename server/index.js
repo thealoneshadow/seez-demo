103b09f5d37115f2f1cc9469d27362c60ea57d50
divyanshuelred <divyanshu@elred.io>
2024-05-19 21:08:28 +0530

    Backend

--- server/index.js
+++ server/index.js
@@ -1,21 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 dotenv.config();
 const routes = require("./routes/index");
-const MONGODB_URI =process.env.MONGODB_URI;
 
 const app = express();
 app.use(express.json());
-app.use(bodyParser.json({limit : "25mb"}));
-app.use(bodyParser.urlencoded({extended: true, limit : "25mb"}));
+app.use(bodyParser.json({ limit: "25mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "25mb" }));
 app.use(cors());
-app.use("/",routes);
+app.use("/", routes);
 
-mongoose.connect(MONGODB_URI).then(result => {
-  console.log('App is listening on url http://localhost:' + process.env.PORT);
-  app.listen(process.env.PORT||5000); 
-})
-.catch(err => console.log(err));
+mongoose
+  .connect(
+    "mongodb+srv://alonesoul2307:alonesoul2307@cluster0.qiujtss.mongodb.net/seez"
+  )
+  .then((result) => {
+    console.log("App is listening on url http://localhost:" + process.env.PORT);
+    app.listen(5000);
+  })
+  .catch((err) => console.log(err));
